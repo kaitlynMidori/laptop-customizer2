34bfcf235e40fd57ff6e4d3c5f9dc22752606e2e
Kaitlyn Midori <katyluhoo@yahoo.com>
2020-11-29 16:18:17 -0800

    initial commit

---
+++ src/components/FeatureSet.js
@@ -0,0 +1,30 @@
+//Main Form
+
+import React, { Component } from 'react';
+import FeatureItem from './FeatureItem';
+
+export default class FeatureSet extends Component {
+  render() {
+    const features = Object.keys(this.props.features).map((feature, idx) => {
+      const featureHash = feature + '-' + idx;
+
+      return (
+        <FeatureItem
+          featureHash={featureHash}
+          feature={feature}
+          featureOption={this.props.features[feature]}
+          selectedFeature={this.props.selected[feature]}
+          updateFeature={(feature, newValue) => this.props.updateFeature(feature, newValue)}
+          USCurrencyFormat={this.props.USCurrencyFormat}
+        />
+      )
+    })
+
+    return (
+      <form className="main__form">
+        <h2>Customize your laptop</h2>
+        {features}
+      </form>
+    )
+  }
+}
